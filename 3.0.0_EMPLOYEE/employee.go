38523573ee9227a707984087354599d65e12b10d
Hemant <hemant@MacBook.local>
2022-11-17 19:06:33 +0530

    Added receiver function with parameters

--- 3.0.0_EMPLOYEE/employee.go
+++ 3.0.0_EMPLOYEE/employee.go
@@ -3,9 +3,27 @@ package main
 type Employee struct {
 	name        string
 	leavesTaken int
+	salary      int
 }
 
 func (emp Employee) getLeavesLeft() int {
 	var leavesLeft = 20 - emp.leavesTaken
 	return leavesLeft
+}
+
+/*
+* If salary-bills > 1L, tax = 10%
+* If salary-bills < 1L, no tax
+ */
+func (emp Employee) getInHandSalary(bills int) float64 {
+	var taxableSalary = emp.salary - bills
+
+	println("The total Taxable salary = ", taxableSalary)
+	if taxableSalary > 100_000 {
+		println("10% tax will be applied on taxable salary = ", taxableSalary)
+		return float64(taxableSalary) * float64(0.9)
+	} else {
+		println("No tax is applicable on taxable salary =", taxableSalary)
+		return float64(taxableSalary)
+	}
 }
--- 3.0.0_EMPLOYEE/main.go
+++ 3.0.0_EMPLOYEE/main.go
@@ -6,11 +6,14 @@ import "fmt"
 TO run - go run main.go employee.go
 */
 func main() {
-	var emp1 = Employee{name: "Hemant", leavesTaken: 4}
+	var emp1 = Employee{name: "Hemant", leavesTaken: 4, salary: 230000}
 
 	fmt.Println(emp1)
 	var left = emp1.getLeavesLeft()
+	fmt.Println("Leaves left = ", left)
 
-	fmt.Println(left)
+	var bills = 45000
+	var inHandSalary = emp1.getInHandSalary(bills)
+	fmt.Println("InHand salary = ", inHandSalary)
 
 }
